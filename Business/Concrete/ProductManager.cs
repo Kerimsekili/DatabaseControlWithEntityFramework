d30a2df7bd66bf0ecdb55c364ed6c31ed6fd47a3
ABDÜLKERİM SEKİLİ <19030411042@ybu.edu.tr>
2022-07-23 17:53:14 +0300

    Adding Autofac, Adding Validator, Adding CrossCuttingConcerns Adding Interceptors

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac;
+using Core.CrossCuttingConcerns;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
+using ValidationException = FluentValidation.ValidationException;
 
 namespace Business.Concrete
 {
@@ -52,13 +58,10 @@ namespace Business.Concrete
              return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
          }
 
+
+        [ValidationAspect(typeof(ProductValidator))]
          public IResult Add(Product product)
          {
-             if (product.ProductName.Length<2)
-             {
-                 //Magic String
-                 return new ErrorResult(Messeges.ProductNameInvalid);
-             }
              _productDal.Add(product);
              return new SuccessResult(Messeges.ProductAdded);
          }
